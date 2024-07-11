4e0b956e0ce9f590dcc8e94b0627ee41d0768ea5
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-11 20:50:12 +0500

    class 5 Arrays and it's methods

---
+++ Arrays/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(students[1]);
+var sum1 = students[3] + students[5];
+// console.log(sum1);
+var stdName = ["Ali", "Ahmed", "Asad", "Ahsan"];
+// console.log(stdName[2].toUpperCase());
+var newArr = new Array(10);
+newArr[0] = 10;
+newArr[1] = 20;
+newArr[2] = 30;
+console.log(newArr);
---
+++ Arrays/index.ts
@@ -0,0 +1,14 @@
+var students:number[] = [1,2,3,4,5,6,7,8,9,10];
+// console.log(students[1]);
+
+var sum1:number = students[3] + students[5];
+// console.log(sum1);
+
+var stdName:string[] = ["Ali", "Ahmed", "Asad", "Ahsan"];
+// console.log(stdName[2].toUpperCase());
+
+var newArr:number[] = new Array(10);
+newArr[0] = 10;
+newArr[1] = 20;
+newArr[2] = 30;
+console.log(newArr);
